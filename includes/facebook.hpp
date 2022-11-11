bc60763f69f8415aabfc0c438c184cbb3651866f
Ilana Kaven <ilanaak2@illinois.edu>
2022-11-11 00:00:23 -0600

    updates to read in file

--- includes/facebook.hpp
+++ includes/facebook.hpp
@@ -8,9 +8,21 @@
 #include <set>
 #include <string>
 #include <vector>
+using namespace std;
 
 class Facebook
-{
+{   
+    public:
+    //constructor 
+    Facebook(const std::string& connections);
+    std::vector<std::string> Breakln(const std::string& str, char delimiter);
+
+
+
+    private:
+    //used to read in file data
+    std::vector<std::vector<std::string>> adj_vect_;
+
 };
 
 #endif
--- src/facebook.cc
+++ src/facebook.cc
@@ -1,1 +1,30 @@
 #include "facebook.hpp"
+
+Facebook::Facebook(const std::string& connections) {  ///reading in a file
+  std::ifstream ifs{connections}; //take in file
+
+  for (std::string line; std::getline(ifs, line); line = "") {
+    std::vector<std::string> get_substr_vector = Breakln(line, ' ');
+    int keep_first = stoi(get_substr_vector.at(0));
+    adj_vect_.push_back(keep_first);
+  }
+
+
+std::vector<std::string> Facebook::Breakln(const std::string& str, char delimiter) {
+    size_t last = 0;
+    std::vector<std::string> substrs;
+    for (size_t i = 0; i != str.length(); ++i) {
+        if (str.at(i) == delimiter || str.at(i) == '\r') {
+        std::string substr = str.substr(last, i - last);
+        last = i + 1;
+        substrs.push_back(substr);
+        }
+    }
+
+    if (last != str.size()) {
+        std::string substr = str.substr(last, str.length() - last);
+        substrs.push_back(substr);
+    }
+
+    return substrs;
+}
